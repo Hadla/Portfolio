07da7b940812d6d56c4967005e3b42ebeaf34899
Hadla <hadla.bergman@gmail.com>
2020-06-02 15:32:53 +0200

    Created Routes to different pages (Home, About, Contact).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Rout, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap/';
 import './App.css';
 
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
   constructor(props) {
@@ -45,6 +48,10 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
 
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ContactPage(props) {
+    return(
+        <p>Contact me!</p>
+    )
+}
+
+export default ContactPage;
