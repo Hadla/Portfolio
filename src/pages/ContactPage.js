b6dd98487467ca2943a4842b4683fe92370011ec
Salt-Admin <salt-admin@Salt-Admins-MacBook-Pro.local>
2020-06-25 14:50:41 +0200

    Created ContactPage. Integraded SendGrid. Have a 401 (Unauthorized) problem.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,72 +3,109 @@ import Content from '../components/Content';
 import { Form, Button } from 'react-bootstrap';
 
 import Hero from '../components/Hero';
-
-
+import Axios from 'axios';
 
 class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      disabled: false,
+      emailSent: null,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      disabled: true,
+    });
+
+    Axios.post('http://localhost:3000/api/email', this.state)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
             disabled: false,
-            emailSent: null,
+            emailSent: true,
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false,
+          });
         }
-    }
-
-
-    handleChange = (event) => {
-        
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+      })
+      .catch((err) => {
+        console.log(err);
 
         this.setState({
-            [name]: value
+          disabled: false,
+          emailSent: false,
         });
-    }
+      });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+  render() {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor='full-name'>Full Name</Form.Label>
+              <Form.Control
+                id='full-name'
+                name='name'
+                type='text'
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor='email'>Email</Form.Label>
+              <Form.Control
+                id='email'
+                name='email'
+                type='email'
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor='message'>Message</Form.Label>
+              <Form.Control
+                id='message'
+                name='message'
+                as='textarea'
+                rows='3'
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Button className='d-inline-block' variant='primary' type='submit' disabled={this.state.disabled}>
+              Send
+            </Button>
 
-        this.setState({
-            disabled: true,
-            emailSent: true
-        });
-    }
-
-    render() {
-    return(
-       <div>
-           <Hero title={this.props.title}/>
-            <Content>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group>
-                        <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                        <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label htmlFor="email">Email</Form.Label>
-                        <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label htmlFor="message">Message</Form.Label>
-                        <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
-                        Send
-                    </Button>
-
-                    {this.state.emailSent === true && <p className="d-inline success-msg">Email was sent successfully!</p>}
-                    {this.state.emailSent === false && <p className="d-inline err-msg">Email was not sent</p>}
-                </Form>
-            </Content>
-       </div>
+            {this.state.emailSent === true && <p className='d-inline success-msg'>Email was sent successfully!</p>}
+            {this.state.emailSent === false && <p className='d-inline err-msg'>Email was not sent</p>}
+          </Form>
+        </Content>
+      </div>
     );
-    }
+  }
 }
 
 export default ContactPage;
